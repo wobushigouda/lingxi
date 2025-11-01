0f6c42187b7671df06c7a8556be7c35fc46435ac
gouda <3296257965@qq.com>
2025-11-02 01:39:12 +0800

    提交前端,并且修改mock

--- isi/isi-ui/src/app.tsx
+++ isi/isi-ui/src/app.tsx
@@ -151,6 +151,7 @@ export const layout: RunTimeLayoutConfig = ({
  * @doc https://umijs.org/docs/max/request#配置
  */
 export const request: RequestConfig = {
-  baseURL: 'https://proapi.azurewebsites.net',
+  // baseURL: 'https://proapi.azurewebsites.net',
+  baseURL: 'http://localhost:8080/',
   ...errorConfig,
 };
