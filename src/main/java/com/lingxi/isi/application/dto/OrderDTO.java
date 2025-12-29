2ac8574f24ada11c35c1aea360ac634b7b99d2b0
gouda <3296257965@qq.com>
2025-12-29 11:22:49 +0800

    初始化提交2

--- src/main/java/com/lingxi/isi/application/dto/OrderDTO.java
+++ src/main/java/com/lingxi/isi/application/dto/OrderDTO.java
@@ -1,5 +1,6 @@
 package com.lingxi.isi.application.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,7 @@ import java.util.List;
  */
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class OrderDTO {
     
     /**
